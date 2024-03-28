922298bee81bd2b04e078d8770b0ce774f94a118
Bhuvan Chandra Mothe <152079724+BhuvanChandraMothe@users.noreply.github.com>
2024-03-28 13:48:35 +0530

    First Commit

---
+++ blogapp/core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse
+
+from blog.models import Post
+
+# Create your views here.
+def frontpage(request):
+    posts = Post.objects.filter(status=Post.ACTIVE)
+
+    return render(request, 'core/frontpage.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def test(request):
+    return render(request, 'core/test.html')
+
+def robots_txt(request):
+    text = [
+        "User-Agent: *",
+        "Disallow: /admin/",
+
+    ]
+    return HttpResponse("\n".join(text), content_type="text/plain") 
+
+
+#def blog(request):
+#    return render(request, 'core/blog.html')
